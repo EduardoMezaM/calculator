ebefd76fd0b14a3d552670a6292d4ea767ceb5e2
EduardoM <eduardomeza540@gmail.com>
2025-05-18 11:33:46 -0700

    Selected all needed DOM elements to be manipulated.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
-console.log('linked correctly')
+const displayOutput = document.querySelector('.output');
+const operatorBtns = document.querySelectorAll('.operatorBtn');
+const digitBtns = document.querySelectorAll('.digitBtn');
+const clearBtn = document.querySelector('.clearBtn');
+const posNegBtn = document.querySelector('.posNegBtn');
+const equalsBtn = document.querySelector('.equalsBtn');
