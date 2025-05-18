c0666b4014465678bfc8bc0de07d88d360f2eca0
EduardoM <eduardomeza540@gmail.com>
2025-05-18 12:08:26 -0700

    added event listener to operator and digit buttons

--- javascript.js
+++ javascript.js
@@ -4,3 +4,33 @@ const digitBtns = document.querySelectorAll('.digitBtn');
 const clearBtn = document.querySelector('.clearBtn');
 const posNegBtn = document.querySelector('.posNegBtn');
 const equalsBtn = document.querySelector('.equalsBtn');
+
+let prevNum = '';
+let currNum = '';
+let operator = '';
+let result = '';
+
+function storeNum(){
+    const digit = this.innerText;
+    if(digit === '.' && currNum.includes('.')) return;
+    else{
+        currNum += digit;
+        displayOutput.innerText = currNum;
+    }
+};
+
+function storeOperator(){
+    prevNum = currNum;
+    operator = this.innerText;
+    currNum = '';
+    displayOutput.innerText = '';
+};
+
+digitBtns.forEach(digitBtn => {
+    digitBtn.addEventListener('click', storeNum);
+});
+
+operatorBtns.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', storeOperator)
+});
+
